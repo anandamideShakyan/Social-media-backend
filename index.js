7b34ae44d3a09974e092b782eb889b477134fd3f
Shakyan Kushwaha <32445019+anandamideShakyan@users.noreply.github.com>
2022-12-29 22:56:17 +0530

    Update index.js

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ passport.use(
 		{
 			clientID: process.env.CLIENT_ID,
 			clientSecret: process.env.CLIENT_SECRET,
-			callbackURL: `${process.env.CLIENT_URL}/auth/google/callback`,
+			callbackURL: `http://localhost:5001/auth/google/callback`,
 			userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo",
 			scope: ["profile"],
 			state: "pass-through value"
